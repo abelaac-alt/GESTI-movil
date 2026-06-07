27d7854c3029ed0ec0e0c61fc0cb2ba0a7857c06
abelaac-alt <abel.atero@gmail.com>
2026-06-07 22:25:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,79 @@
-const CACHE_NAME = "finanzas-cache-v3";
+const CACHE_NAME = 'gestimovil-v1.0.0';
 
-const ASSETS = [
-  "/Finanzas-personales/",
-  "/Finanzas-personales/index.html",
-  "/Finanzas-personales/manifest.json",
-  "/Finanzas-personales/icon-192.png",
-  "/Finanzas-personales/icon-512.png"
+const FILES_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+
+    self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
+    );
+
+    self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+self.addEventListener('fetch', (event) => {
+
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request)
+                    .then(networkResponse => {
+
+                        if (
+                            !networkResponse ||
+                            networkResponse.status !== 200 ||
+                            networkResponse.type !== 'basic'
+                        ) {
+                            return networkResponse;
+                        }
+
+                        const responseClone = networkResponse.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseClone);
+                            });
+
+                        return networkResponse;
+                    });
+            })
+            .catch(() => {
+
+                if (event.request.destination === 'document') {
+                    return caches.match('./index.html');
+                }
+
+            })
+    );
 });
